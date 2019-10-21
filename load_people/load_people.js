d9c5bb9ce41988db4e730a39f1c11c28b62545cd
rapPayne <bob@creator.net>
2019-10-21 10:19:33 -0500

    Added load_people Node script

---
+++ load_people/load_people.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+const admin = require('firebase-admin');
+
+let serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+const firebaseProjectId = 'learning-flutter-e3d0a';
+
+loadRandomPeople(5);
+
+async function loadRandomPeople(numberOfPeople=10) {
+ let randomPeople;
+ const url = `http://randomuser.me/api?results=${numberOfPeople}`;
+ fetch(url)
+  .then(res => res.json())
+  .then(res => res.results)
+  .then(people => {
+   for (let person of people) {
+    addPersonToFirestore(person);
+   }
+  })
+  .catch(console.error);
+}
+
+function addPersonToFirestore(person) {
+ const insertUrl = `https://firestore.googleapis.com/v1/projects/${firebaseProjectId}/databases/(default)/documents/people`;
+ db.collection('people')
+ .add(person)
+ .then(ref => console.log(`Added person ${ref} - ${person.name.first} ${person.name.last} `));
+}
